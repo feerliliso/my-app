33cb97df44d70e629cb3dcfadc63c570ae6429d5
feerliliso <feerliliso@hotmail.com>
2018-05-07 11:02:35 +0800

    add new severce
    rt

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import {hero} from './hero';
+import {HEROES} from './mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes():hero[]{
+    return HEROES;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {HeroService} from '../hero.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-heroes=HEROES;
+heroes:Hero[];
 selectedHero:Hero;
 
-  constructor() { }
+getHeroes(): void {
+  this.heroes = this.heroService.getHeroes();
+}
 
-  ngOnInit() {}
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
   onSelect(hero:Hero):void{
     this.selectedHero = hero;
   }
